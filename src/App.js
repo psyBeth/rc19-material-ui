e34b2bf3b531939ff00aff19110e26fb8fea8952
Betul <betulkoru06@gmail.com>
2024-05-18 10:18:17 +0300

    app bar + (manifest err will be fixed)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Home from "./pages/Home";
 import { createTheme, ThemeProvider } from "@mui/material";
-import { deepOrange, deepPurple, teal } from "@mui/material/colors";
+import { deepPurple, teal } from "@mui/material/colors";
 
 function App() {
 
   const theme = createTheme({
-    
+
     palette: {
       primary: {
-        main: deepOrange[500],
+        main: "#dc143c",
       },
       secondary: {
         main: teal[200],
@@ -22,7 +22,11 @@ function App() {
 
     breakpoints: {
       values: {
-        sm: "650",
+        xs: 0,
+        sm: 500,
+        md: 800,
+        lg: 1200,
+        xl: 1536,
       },
     },
 
